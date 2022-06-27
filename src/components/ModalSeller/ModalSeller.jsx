caa783cd093e7e98394302b05042cb6413f6944e
MohammadHidayatullah <mohammadhidayatullah85@gmail.com>
2022-06-27 20:54:16 +0700

    update responsive dan push modal seller status

--- src/components/ModalSeller/ModalSeller.jsx
+++ src/components/ModalSeller/ModalSeller.jsx
@@ -4,17 +4,27 @@ import React from "react";
 import person from "../../assets/img/person.svg";
 import produk from "../../assets/img/produk.jpg";
 import { FaWhatsapp } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
 import NumberFormat from "react-number-format";
 
 function ModalSeller() {
   return (
     <>
-      <div class='modal' id='modalSeller'>
+      <div className='modal' id='modalSeller'>
         <div
-          class='modal-dialog modal-dialog-centered'
-          style={{ width: "25%", borderRadius: "16px" }}>
-          <div class='modal-content' style={{ padding: 32 }}>
-            <p class='modal-title' style={{ fontWeight: "bold" }}>
+          className='modal-dialog modal-dialog-centered'
+          style={{ width: "360px", margin: "0 auto" }}>
+          <div
+            className='modal-content'
+            style={{ padding: 32, borderRadius: "16px" }}>
+            <div className='row text-end' style={{ marginInlineStart: "auto" }}>
+              <FiX
+                size={22}
+                data-bs-dismiss='modal'
+                style={{ cursor: "pointer" }}
+              />
+            </div>
+            <p className='modal-title my-2' style={{ fontWeight: "bold" }}>
               Yeay kamu berhasil mendapat harga <br /> yang sesuai
             </p>
             <p style={{ color: "#8A8A8A" }}>
@@ -23,59 +33,65 @@ function ModalSeller() {
             </p>
 
             <div
-              class='modal-body mb-3'
+              className='modal-body mb-4'
               style={{ background: "#EEEEEE", borderRadius: "16px" }}>
-              <div class='row'>
+              <div className='row'>
                 <div className='col-12 text-center'>
                   <p style={{ fontWeight: "bold" }}>Product Match</p>
                 </div>
-                <div class='col-sm-3'>
+                <div className='d-flex ps-0 pe-0 mb-2'>
                   <img
+                    className='mx-3'
                     src={person}
                     alt='person'
                     style={{ width: "48px", borderRadius: "12px" }}
                   />
+                  <div className='C'>
+                    <p style={{ margin: "0", fontWeight: "bold" }}>
+                      Nama Pembeli
+                    </p>
+                    <p>Kota</p>
+                  </div>
                 </div>
-                <div class='col-sm-8'>
-                  <p style={{ margin: "0", fontWeight: "bold" }}>
-                    Nama Pembeli
-                  </p>
-                  <p>Kota</p>
-                </div>
-                <div class='col-sm-3'>
+                <div className='d-flex ps-0 pe-0'>
                   <img
-                    style={{ width: "48px", borderRadius: "12px" }}
+                    className='mx-3'
+                    style={{
+                      width: "48px",
+                      height: "48px",
+                      borderRadius: "12px",
+                    }}
                     src={produk}
                     alt='produk'
                   />
-                </div>
-                <div class='col-sm-8'>
-                  <p style={{ margin: "0" }}>Nike</p>
-                  <p style={{ margin: "0", textDecoration: "line-through" }}>
-                    <NumberFormat
-                      value={250000}
-                      displayType={"text"}
-                      thousandSeparator={"."}
-                      decimalSeparator={","}
-                      prefix={"Rp "}
-                    />
-                  </p>
-                  <p>
-                    Ditawar{" "}
-                    <NumberFormat
-                      value={200000}
-                      displayType={"text"}
-                      thousandSeparator={"."}
-                      decimalSeparator={","}
-                      prefix={"Rp "}
-                    />
-                  </p>
+                  <div className='C'>
+                    <p style={{ margin: "0" }}>Nike</p>
+                    <p style={{ margin: "0", textDecoration: "line-through" }}>
+                      <NumberFormat
+                        value={250000}
+                        displayType={"text"}
+                        thousandSeparator={"."}
+                        decimalSeparator={","}
+                        prefix={"Rp "}
+                      />
+                    </p>
+                    <p>
+                      Ditawar{" "}
+                      <NumberFormat
+                        value={200000}
+                        displayType={"text"}
+                        thousandSeparator={"."}
+                        decimalSeparator={","}
+                        prefix={"Rp "}
+                      />
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
             <button
               type='button'
-              class='btn tawar'
+              className='btn tawar'
               data-bs-dismiss='modal'
               style={{
                 background: "#181818",
--- src/components/ModalSellerStatus/ModalSellerStatus.jsx
+++ src/components/ModalSellerStatus/ModalSellerStatus.jsx
@@ -1,51 +1,67 @@
 /** @format */
 
 import React from "react";
-import './ModalSellerStatus.css';
+import "./ModalSellerStatus.css";
+import { FiX } from "react-icons/fi";
 
 function ModalSellerStatus() {
   return (
     <>
-      <div class='modal' id='modalSellerStatus'>
+      <div className='modal' id='modalSellerStatus'>
         <div
-          class='modal-dialog modal-dialog-centered'
-          style={{ width: "25%", borderRadius: "16px" }}>
-          <div class='modal-content' style={{ padding: 32 }}>
-            <p class='modal-title text-center' style={{ fontWeight: "bold" }}>
+          className='modal-dialog modal-dialog-centered'
+          style={{ width: "360px", margin: "0 auto" }}>
+          <div
+            className='modal-content'
+            style={{ padding: 32, borderRadius: "16px" }}>
+            <div className='row text-end' style={{ marginInlineStart: "auto" }}>
+              <FiX
+                size={22}
+                data-bs-dismiss='modal'
+                style={{ cursor: "pointer" }}
+              />
+            </div>
+            <p className='modal-title mb-3' style={{ fontWeight: "bold" }}>
               Perbarui status penjualan produkmu
             </p>
 
             <div
-              class='modal-body mb-3'
-            //   style={{ background: "#EEEEEE", borderRadius: "16px" }}
-              >
-              <div class='form-check'>
+              className='modal-body mb-3 p-0'
+              //   style={{ background: "#EEEEEE", borderRadius: "16px" }}
+            >
+              <div className='form-check mb-2'>
                 <input
-                  class='form-check-input'
+                  className='form-check-input'
                   type='radio'
                   name='flexRadioDefault'
                   id='flexRadioDefault1'
                 />
-                <label class='form-check-label' for='flexRadioDefault1'>
-                  <span>Berhasil terjual</span>
+                <label className='form-check-label' for='flexRadioDefault1'>
+                  Berhasil terjual
+                  <span>
+                    Kamu telah sepakat menjual produk ini kepada pembeli
+                  </span>
                 </label>
               </div>
-              <div class='form-check'>
+              <div className='form-check'>
                 <input
-                  class='form-check-input'
+                  className='form-check-input'
                   type='radio'
                   name='flexRadioDefault'
                   id='flexRadioDefault2'
                   checked
                 />
-                <label class='form-check-label' for='flexRadioDefault2'>
+                <label className='form-check-label' for='flexRadioDefault2'>
                   Batalkan transaksi
+                  <span>
+                    Kamu membatalkan transaksi produk ini dengan pembeli
+                  </span>
                 </label>
               </div>
             </div>
             <button
               type='button'
-              class='btn tawar'
+              className='btn tawar'
               data-bs-dismiss='modal'
               style={{
                 background: "#181818",
