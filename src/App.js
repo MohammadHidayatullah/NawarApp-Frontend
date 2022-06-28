8ab578dde5006dd92a647a4a9eb90be508c7bb48
MohammadIrfanNugraha <irfannugraha11016@gmail.com>
2022-06-28 20:27:28 +0700

    Up fitur info produk buyer and seller

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from "./pages/Home/Home";
 import InfoProfil from "./pages/InfoProfil/InfoProfil";
 import InfoProduk from "./pages/InfoProduk/InfoProduk";
 import DaftarJual from "./pages/DaftarJual/DaftarJual";
+import InfoProdukPage from "./pages/InfoProdukPage/InfoProdukPage";
+import InfoProdukSeller from "./pages/InfoProdukSeller/InfoProdukSeller";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -15,6 +17,8 @@ function App() {
       <Route path="/info-profil" element={<InfoProfil />}></Route>
       <Route path="/info-produk" element={<InfoProduk />}></Route>
       <Route path="/daftar-jual" element={<DaftarJual />}></Route>
+      <Route path="/info-produk-page" element={<InfoProdukPage />}></Route>
+      <Route path="/info-produk-seller" element={<InfoProdukSeller />}></Route>
     </Routes>
   );
 }
