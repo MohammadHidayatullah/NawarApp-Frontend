f4cc5433420b3a34f7b1f1300496894e898bdcdd
MohammadHidayatullah <mohammadhidayatullah85@gmail.com>
2022-06-29 20:08:56 +0700

    set-up redux

--- src/App.js
+++ src/App.js
@@ -13,20 +13,26 @@ import "slick-carousel/slick/slick-theme.css";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import InfoPenawaran from "./pages/InfoPenawaran/InfoPenawaran";
+import { Provider } from "react-redux";
+import store from './redux/store';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='/info-profil' element={<InfoProfil />}></Route>
-      <Route path='/info-produk' element={<InfoProduk />}></Route>
-      <Route path='/daftar-jual' element={<DaftarJual />}></Route>
-      <Route path='/info-produk-page' element={<InfoProdukPage />}></Route>
-      <Route path='/info-produk-seller' element={<InfoProdukSeller />}></Route>
-      <Route path='/login' element={<Login />}></Route>
-      <Route path='/register' element={<Register />}></Route>
-      <Route path='/info-penawaran' element={<InfoPenawaran />}></Route>
-    </Routes>
+    <Provider store={store}>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/info-profil' element={<InfoProfil />}></Route>
+        <Route path='/info-produk' element={<InfoProduk />}></Route>
+        <Route path='/daftar-jual' element={<DaftarJual />}></Route>
+        <Route path='/info-produk-page' element={<InfoProdukPage />}></Route>
+        <Route
+          path='/info-produk-seller'
+          element={<InfoProdukSeller />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/register' element={<Register />}></Route>
+        <Route path='/info-penawaran' element={<InfoPenawaran />}></Route>
+      </Routes>
+    </Provider>
   );
 }
 
