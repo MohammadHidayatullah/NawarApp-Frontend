e620e997e51e00ea3d986262be4feb180f309f52
Yobato <satriyobagus30077@gmail.com>
2022-07-04 22:14:59 +0700

    Peralihan dari cookies ke local storage

--- src/redux/action/authAction.js
+++ src/redux/action/authAction.js
@@ -2,17 +2,22 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 export const authLogin = (email, password, navigate) => async (dispatch) => {
-  let inOneHours = new Date(new Date().getTime() + 60 * 60 * 1000);
+  // let inOneHours = new Date(new Date().getTime() + 60 * 60 * 1000);
+  let token = localStorage.getItem("token");
   try {
     dispatch({ type: `LOADING` });
     let res = await axios({
       method: "POST",
       url: "https://nawar-api.herokuapp.com/api/v1/auth/login",
       data: { email: email, password: password },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
     });
     console.log(res);
     if (res.status === 200) {
-      Cookies.set("token", res.data.data.token, { expires: inOneHours });
+      // Cookies.set("token", res.data.data.token, { expires: inOneHours });
+      localStorage.setItem("token", res.data.data.token);
       navigate("/");
     }
     // .then((res) => {
