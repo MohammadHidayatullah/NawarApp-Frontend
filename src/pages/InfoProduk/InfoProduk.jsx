a20ff796183789714cd049a60c21590344dbd2ce
Yobato <satriyobagus30077@gmail.com>
2022-07-12 22:21:18 +0700

    Multi kategori fixed, error 500

--- src/pages/InfoProduk/InfoProduk.jsx
+++ src/pages/InfoProduk/InfoProduk.jsx
@@ -63,7 +63,7 @@ function InfoProduk() {
   const [name, setName] = useState();
   const [price, setPrice] = useState();
   const [description, setDescription] = useState();
-  const [category, setCategory] = useState([]);
+  const [category, setCategory] = useState();
   const [size, setSize] = useState();
   // const [id, setId] = useState();
 
@@ -87,6 +87,15 @@ function InfoProduk() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    const data = {
+      images,
+      name,
+      price,
+      description,
+      category,
+      size,
+    };
+
     let formData = new FormData();
 
     // Loop untuk append gambar (Karena gambar bukan array)
@@ -99,6 +108,8 @@ function InfoProduk() {
     formData.append("description", description);
     formData.append("category", category);
     formData.append("size", size);
+
+    console.log(data);
     // dispatch(createProduct(formData));
     // resetForm();
   };
@@ -121,10 +132,6 @@ function InfoProduk() {
 
     dispatch(draftProduct(formData));
     resetForm();
-  };
-
-  const handleChange = (value) => {
-    setCategory(Array.from(value.selectedOptions, (option) => option.value));
   };
 
   // Fungsi untuk handle responsive mobile
@@ -154,63 +161,71 @@ function InfoProduk() {
   // Fungsi redux
   return (
     <>
-      {width >= 576 && <Navbar />}
-      <div style={{ padding: "3%" }}>
-        <div className="container" style={{ maxWidth: "568px" }}>
-          <button className="d-none d-sm-block arrow-left" onClick={navHome}>
-            <FiArrowLeft size={22} />
-          </button>
-          {width <= 576 && (
-            <div className="d-flex justify-content-between">
-              <button className=" arrow-left" onClick={navHome}>
-                <FiArrowLeft size={22} />
-              </button>
-              <p className="mb-4" style={{ marginLeft: "-28.84px" }}>
-                <b>Lengkapi Detail Produk</b>
-              </p>
-              <p></p>
-            </div>
-          )}
-          <div className="row">
-            <div className="col-lg-12">
-              <form>
-                <div className="mb-3">
-                  <label className="form-label">Nama Produk</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="namaProduk"
-                    aria-describedby="namaHelp"
-                    placeholder="Nama Produk"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                  />
-                </div>
-                <div className="mb-3">
-                  <label className="form-label">Harga Produk</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="examplePrice"
-                    aria-describedby="priceHelp"
-                    placeholder="Rp 0,00"
-                    value={price}
-                    onChange={(e) => setPrice(e.target.value)}
-                  />
-                </div>
-                <div className="mb-3">
-                  <label className="form-label">Kategori</label>
-                  <Select
-                    id="kategoriProduk"
-                    closeMenuOnSelect={false}
-                    // components={animatedComponents}
-                    // defaultValue={[colourOptions[4], colourOptions[5]]}
-                    isMulti
-                    value={category}
-                    options={options}
-                    onChange={handleChange}
-                  />
-                  {/* <select
+      <section>
+        {width >= 576 && <Navbar />}
+        <div style={{ padding: "3%" }}>
+          <div className="container" style={{ maxWidth: "568px" }}>
+            <button className="d-none d-sm-block arrow-left" onClick={navHome}>
+              <FiArrowLeft size={22} />
+            </button>
+            {width <= 576 && (
+              <div className="d-flex justify-content-between">
+                <button className=" arrow-left" onClick={navHome}>
+                  <FiArrowLeft size={22} />
+                </button>
+                <p className="mb-4" style={{ marginLeft: "-28.84px" }}>
+                  <b>Lengkapi Detail Produk</b>
+                </p>
+                <p></p>
+              </div>
+            )}
+            <div className="row">
+              <div className="col-lg-12">
+                <form>
+                  <div className="mb-3">
+                    <label className="form-label">Nama Produk</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="namaProduk"
+                      aria-describedby="namaHelp"
+                      placeholder="Nama Produk"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Harga Produk</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="examplePrice"
+                      aria-describedby="priceHelp"
+                      placeholder="Rp 0,00"
+                      value={price}
+                      onChange={(e) => setPrice(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Kategori</label>
+                    <Select
+                      id="kategoriProduk"
+                      closeMenuOnSelect={false}
+                      // components={animatedComponents}
+                      // defaultValue={[colourOptions[4], colourOptions[5]]}
+                      isMulti
+                      // value={category}
+                      options={options}
+                      onChange={(val) => {
+                        const dataCategory = val.map((id) => {
+                          // console.log(id.value);
+                          return id.value;
+                        });
+                        // console.log(dataCategory);
+                        setCategory(dataCategory);
+                      }}
+                    />
+                    {/* <select
                     className="form-select"
                     aria-label=".form-select-sm multiple select example"
                     style={{ height: "28,5pt" }}
@@ -232,117 +247,118 @@ function InfoProduk() {
                         );
                       })
                     )} */}
-                  {/* <option value="1">One</option>
+                    {/* <option value="1">One</option>
                     <option value="2">Two</option>
                     <option value="3">Three</option> */}
-                  {/* </select> */}
-                </div>
-                <div className="mb-3">
-                  <label className="form-label">Ukuran Sepatu</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="exampleUkuran"
-                    aria-describedby="sizeHelp"
-                    placeholder="40"
-                    value={size}
-                    onChange={(e) => setSize(e.target.value)}
-                  />
-                </div>
-                <div className="mb-3">
-                  <label className="form-label">Deskripsi</label>
-                  <textarea
-                    class="form-control"
-                    id="exampleFormControlTextarea1"
-                    rows="3"
-                    placeholder="Contoh: Jalan Ikan Hiu 33"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                  ></textarea>
-                </div>
-                <div className="mb-3">
-                  <label className="form-label">Foto Produk</label>
-                  {selectedImages.length <= 10 ? (
-                    ""
-                  ) : (
-                    <p className="pt-3">
-                      Anda hanya boleh upload 10 gambar! Hapus{" "}
-                      <b className="error">
-                        {selectedImages.length - 10} gambar
-                      </b>
-                    </p>
-                  )}
-                  <div className="images">
-                    <div className="grid">
-                      <label className="image-uploader">
-                        <FiPlus />
-                        {/* <br /> */}
-                        <input
-                          className="input-image"
-                          type="file"
-                          name="images"
-                          onChange={onSelectFile}
-                          accept="image/png, image/jpeg, image/webp"
-                        />
-                      </label>
-                    </div>
-                    {selectedImages &&
-                      selectedImages.map((image, index) => {
-                        return (
-                          <div key={index} className="grid">
-                            <div className="image">
-                              <img id="img" src={image} alt="upload" />
-                              <button
-                                onClick={(e) => {
-                                  e.preventDefault();
-                                  console.log(index, "ini index");
-                                  setSelectedImages(
-                                    selectedImages.filter((e) => e !== image)
-                                  );
-                                  setImages(
-                                    images.filter((value, f) => f !== index)
-                                  );
-                                }}
-                              >
-                                <FiX /> Delete
-                              </button>
-                              {/* <p className="pt-1">{index + 1}</p> */}
+                    {/* </select> */}
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Ukuran Sepatu</label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="exampleUkuran"
+                      aria-describedby="sizeHelp"
+                      placeholder="40"
+                      value={size}
+                      onChange={(e) => setSize(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Deskripsi</label>
+                    <textarea
+                      class="form-control"
+                      id="exampleFormControlTextarea1"
+                      rows="3"
+                      placeholder="Contoh: Jalan Ikan Hiu 33"
+                      value={description}
+                      onChange={(e) => setDescription(e.target.value)}
+                    ></textarea>
+                  </div>
+                  <div className="mb-3">
+                    <label className="form-label">Foto Produk</label>
+                    {selectedImages.length <= 10 ? (
+                      ""
+                    ) : (
+                      <p className="pt-3">
+                        Anda hanya boleh upload 10 gambar! Hapus{" "}
+                        <b className="error">
+                          {selectedImages.length - 10} gambar
+                        </b>
+                      </p>
+                    )}
+                    <div className="images">
+                      <div className="grid">
+                        <label className="image-uploader">
+                          <FiPlus />
+                          {/* <br /> */}
+                          <input
+                            className="input-image"
+                            type="file"
+                            name="images"
+                            onChange={onSelectFile}
+                            accept="image/png, image/jpeg, image/webp"
+                          />
+                        </label>
+                      </div>
+                      {selectedImages &&
+                        selectedImages.map((image, index) => {
+                          return (
+                            <div key={index} className="grid">
+                              <div className="image">
+                                <img id="img" src={image} alt="upload" />
+                                <button
+                                  onClick={(e) => {
+                                    e.preventDefault();
+                                    console.log(index, "ini index");
+                                    setSelectedImages(
+                                      selectedImages.filter((e) => e !== image)
+                                    );
+                                    setImages(
+                                      images.filter((value, f) => f !== index)
+                                    );
+                                  }}
+                                >
+                                  <FiX /> Delete
+                                </button>
+                                {/* <p className="pt-1">{index + 1}</p> */}
+                              </div>
                             </div>
-                          </div>
-                        );
-                      })}
-                    {/* <div className="images"></div> */}
+                          );
+                        })}
+                      {/* <div className="images"></div> */}
+                    </div>
+                    {/* <ImageUploadPreviewComponent /> */}
                   </div>
-                  {/* <ImageUploadPreviewComponent /> */}
-                </div>
-                <div className="row">
-                  <div className="col-lg-6">
-                    <button
-                      type="submit"
-                      className="btn btn-outline w-100 mb-3"
-                      style={{ borderColor: "#181818", color: "#181818" }}
-                      onClick={handleDraft}
-                    >
-                      Preview
-                    </button>
+                  <div className="row">
+                    <div className="col-lg-6">
+                      <button
+                        type="submit"
+                        className="btn btn-outline w-100 mb-3"
+                        style={{ borderColor: "#181818", color: "#181818" }}
+                        onClick={handleDraft}
+                      >
+                        Preview
+                      </button>
+                    </div>
+                    <div className="col-lg-6">
+                      <button
+                        type="submit"
+                        className="btn w-100 mb-3"
+                        style={{ backgroundColor: "#181818", color: "#FFFF" }}
+                        // onClick={() => (id ? handleEditSubmit() : handleSubmit())}
+                        onClick={handleSubmit}
+                      >
+                        Terbitkan
+                      </button>
+                    </div>
                   </div>
-                  <div className="col-lg-6">
-                    <button
-                      type="submit"
-                      className="btn w-100 mb-3"
-                      style={{ backgroundColor: "#181818", color: "#FFFF" }}
-                      // onClick={() => (id ? handleEditSubmit() : handleSubmit())}
-                      onClick={handleSubmit}
-                    >
-                      Terbitkan
-                    </button>
-                  </div>
-                </div>
-              </form>
+                </form>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
